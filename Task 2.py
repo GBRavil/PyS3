7ff30caedaa70154200a31ab37193cd1d03c2a67
Ravil <lllx2@yandex.ru>
2022-12-23 14:18:58 +0300

    2

--- Task 2.py
+++ Task 2.py
@@ -1,16 +1,9 @@
 # Напишите программу, которая найдёт произведение пар чисел списка. 
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
 from random import randint
-nums = []
 n = int(input('Введите число '))
-for i in range(n):
-    nums.append(randint(0,n))
+nums = [randint(0,n) for i in range(n)]
 print(f'Список элементов {nums}')
-
 r = round(n / 2 + 0.5) 
-mult = []
-for i in range(r):
-    mult.append(randint(0,r))
-for i in range(r):
-    mult[i] = nums[i] * nums[n-1-i]
-print(f'Произведение пар чисел {mult}')    
+mult = [nums[i]*nums[len(nums)-i-1] for i in range(r)]
+print(f'Произведение пар чисел {mult}')  
