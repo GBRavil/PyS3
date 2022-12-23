7037ddd495b96246101e4cb767911da6cacc0afe
Ravil <lllx2@yandex.ru>
2022-12-23 13:11:22 +0300

    1

---
+++ Task 1.py
@@ -0,0 +1,18 @@
+# Задайте список из нескольких чисел. Напишите программу, 
+# которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+from random import randint
+nums = []
+n = int(input('Введите число '))
+for i in range(n):
+    nums.append(randint(-n,n))
+print('Список элементов',nums)
+sum = 0
+
+for i in range(n):
+    if i % 2 !=0:
+        sum += nums[i]
+    else:
+        sum    
+print(f'Сумма элементов на нечетных позициях {sum}')       
+
+
